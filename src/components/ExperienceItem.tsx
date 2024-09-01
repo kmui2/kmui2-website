7af3ab517e4aa1fbe98dcb4af9cbd70983dbe355
Kevin Mui <kmui2@wisc.edu>
2024-09-01 13:30:04 -0500

    Add hovering for the experience cards

--- src/components/ExperienceItem.tsx
+++ src/components/ExperienceItem.tsx
@@ -9,6 +9,7 @@ import ExperienceList from './ExperienceList';
 import { Badge } from './park-ui/badge';
 import { Icon } from './park-ui/icon';
 import { Text } from './park-ui/text';
+import { Card } from './park-ui/card';
 
 const ExperienceItem: Component<{
   title: string;
@@ -21,52 +22,65 @@ const ExperienceItem: Component<{
   return (
     <Grid gridTemplateColumns="20% min-content auto" w="full">
       <Stack textAlign="right">
-        <Heading as="h3" size="xs" lineHeight="1.875rem" fontWeight="bold">
+        <Heading as="h3" size="xs" lineHeight="1.875rem" fontWeight="bold" mt="0.75rem">
           {props.period}
         </Heading>
       </Stack>
       <Stack align="center" gap={0}>
+        <Box border="solid 1px" borderColor="border.muted" w="0" h="0.75rem" />
         <Center w="1.5rem" h="1.5rem">
           <Circle bg="fg.muted" size="0.5rem" />
         </Center>
         <Box border="solid 1px" borderColor="border.muted" w="0" h="max" flex={1} />
       </Stack>
-      <Stack mb={3}>
-        <Stack gap={0}>
-          <Heading as="h2" size="lg" fontWeight="bold">
-            {props.title} @{' '}
-            <Text as="span" color="red.9">
-              {props.company}
-            </Text>
-          </Heading>
-          <Heading as="h3" size="sm" color="fg.muted">
-            {props.location}
-          </Heading>
-        </Stack>
-        <Stack gap={1}>
-          <ExperienceList title="Summary" description={props.description} />
-          {/* <ExperienceList title="Achievements" description={props.description} />
+      <Card.Root
+        pt={3}
+        mb={3}
+        borderColor="border.muted"
+        border="solid"
+        borderWidth="1px"
+        transition="all 200ms ease-in-out"
+        _hover={{ boxShadow: '2xl', transform: 'scale(1.1)', zIndex: 9999 }}
+      >
+        <Card.Body>
+          <Stack>
+            <Stack gap={0}>
+              <Heading as="h2" size="lg" fontWeight="bold">
+                {props.title} @{' '}
+                <Text as="span" color="red.9">
+                  {props.company}
+                </Text>
+              </Heading>
+              <Heading as="h3" size="sm" color="fg.muted">
+                {props.location}
+              </Heading>
+            </Stack>
+            <Stack gap={1}>
+              <ExperienceList title="Summary" description={props.description} />
+              {/* <ExperienceList title="Achievements" description={props.description} />
           <ExperienceList title="Responsibilities" description={props.description} /> */}
-        </Stack>
-        <HStack>
-          {
-            <For each={props.techStack}>
-              {({ icon, label }) => (
-                <Badge bg="red.4" color="red.9" size="sm">
-                  {icon && (
-                    <Icon
-                      h="1rem"
-                      color="red.9"
-                      asChild={(props) => <Dynamic component={icon} {...props()} />}
-                    />
-                  )}{' '}
-                  {label}
-                </Badge>
-              )}
-            </For>
-          }
-        </HStack>
-      </Stack>
+            </Stack>
+            <HStack>
+              {
+                <For each={props.techStack}>
+                  {({ icon, label }) => (
+                    <Badge bg="red.4" color="red.9" size="sm">
+                      {icon && (
+                        <Icon
+                          h="1rem"
+                          color="red.9"
+                          asChild={(props) => <Dynamic component={icon} {...props()} />}
+                        />
+                      )}{' '}
+                      {label}
+                    </Badge>
+                  )}
+                </For>
+              }
+            </HStack>
+          </Stack>
+        </Card.Body>
+      </Card.Root>
     </Grid>
   );
 };
---
+++ src/components/LeftPanel.tsx
@@ -0,0 +1,79 @@
+import { Heading } from '~/components/park-ui/heading';
+import { Text } from '~/components/park-ui/text';
+import { Box, HStack, Stack, styled } from '~/styled-system/jsx';
+
+import { GlobeIcon, MailIcon, MoonIcon, PhoneIcon, X } from 'lucide-solid';
+import type { Component } from 'solid-js';
+import avatarImage from '~/assets/avatar.jpg';
+import { Link } from '~/components/park-ui/link';
+import { css } from '~/styled-system/css';
+import { SiGithub, SiLinkedin, SiStackoverflow, SiTwitter } from 'solid-icons/si';
+
+const iconMargin = 1;
+
+const LeftPanel: Component = () => {
+  return (
+    <Stack>
+      <Box p={2} w="max" h="max" border="solid" borderColor="red.9" borderWidth={1}>
+        <Box bg="black" p={5} borderRadius="xl">
+          <styled.img
+            src={avatarImage.src}
+            alt="Kevin Mui"
+            class={css({ h: '6rem', w: '6rem', clipPath: 'circle(50%)' })}
+          />
+        </Box>
+      </Box>
+      <Stack gap={0}>
+        <Heading as="h1" size="4xl" fontWeight="bold">
+          Kevin Mui
+        </Heading>
+        <Heading as="h2" size="lg" color="fg.muted">
+          Software Engineer / Full Stack Engineer
+        </Heading>
+      </Stack>
+      <Stack gap={0}>
+        <HStack gap={2} color="fg.muted" alignItems="center">
+          <GlobeIcon size="0.75rem" />
+          <Text color="fg.muted" size="sm">
+            {' '}
+            Chicago, IL
+          </Text>
+        </HStack>
+        <Link href="mailto:kevinmui1997@yahoo.com">
+          <HStack gap={2} color="fg.muted" alignItems="center">
+            <MailIcon size="0.75rem" />
+            <Text size="sm" color="fg.muted">
+              kevinmui1997@yahoo.com
+            </Text>
+          </HStack>
+        </Link>
+      </Stack>
+      <HStack color="red.9">
+        <MailIcon size="1.25rem" class={css({ m: iconMargin })} />
+        <PhoneIcon size="1.25rem" class={css({ m: iconMargin })} />
+        <SiLinkedin size="1.25rem" class={css({ m: iconMargin })} />
+        <SiTwitter size="1.25rem" class={css({ m: iconMargin })} />
+        <SiGithub size="1.25rem" class={css({ m: iconMargin })} />
+        <SiStackoverflow size="1.25rem" class={css({ m: iconMargin })} />
+      </HStack>
+      <Heading as="h1" size="4xl" fontWeight="bold">
+        About
+      </Heading>
+      <Text size="sm" color="neutral.11">
+        As a seasoned Software Engineer with 8 years of senior level experience, specializing in
+        fintech, Java backend, and React frontend development, I have a proven track record of
+        designing and implementing high-volume risk management applications. During my tenure at CME
+        Group for over 5 years, I honed my skills in developing cutting-edge solutions on Google
+        Cloud Platform (GCP). I bring a unique blend of technical expertise and industry-specific
+        knowledge that enables me to drive innovation and deliver exceptional results.
+      </Text>
+
+      <HStack alignItems="center">
+        <MoonIcon />
+        <Text>Dark</Text>
+      </HStack>
+    </Stack>
+  );
+};
+
+export default LeftPanel;
