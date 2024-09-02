e2c4f23bfd6d1c70fc16e235f6c26ec44b3fced8
Kevin Mui <kmui2@wisc.edu>
2024-09-02 00:22:02 -0500

    Add eslint ignore for astro types (it keeps getting added automatically by astro)

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="../.astro/types.d.ts" />
 import '../.astro/types.d.ts';
 declare module '*.ttf' {
   const src: string;
