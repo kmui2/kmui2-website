f25d31b936e94966fddf0cdb2976da359b6bd4be
Kevin Mui <kmui2@wisc.edu>
2024-09-02 10:31:59 -0500

    Use triple /// import to fix file references in react folder

--- src/react/env.d.ts
+++ src/react/env.d.ts
@@ -1,15 +1,7 @@
-import '../.astro/types';
-
 declare module '*.ttf' {
   const src: string;
   export default src;
 }
-
-// declare module '*.yaml' {
-//   const content: any;
-//   export default content;
-// }
-
 declare module '*/content.yaml' {
   const content: {
     skills: string[];
